0806f2e69790fcd26a7fdf2005a6d90512d651ae
Mr Hafi <hafi.cs01@gmail.com>
2026-03-15 03:03:35 +0500

    add guest and location filtering to yacht archive
    Combined all filters into single yacht_sorting() AJAX function
    Added guest filter dropdown with dynamic DB values
    Added location filter with radio buttons from distinct home_port values
    Used $conditions[] array with implode AND for clean multi-filter SQL
    Fixed sidebar output by wrapping in yacht_sidebar() function to prevent raw output on AJAX calls

---
+++ yacht-archive/sidebar/templates/yacht_card.php
@@ -0,0 +1,31 @@
+<?php 
+
+?>
+<div class="col-md-4">
+<div class="card bg-dark text-white border-0 h-100">
+
+<img src="<?php echo esc_url($yacht->main_image_api); ?>" class="card-img-top">
+
+<div class="card-body">
+
+<h5 class="fw-bold text-uppercase">
+<?php echo esc_html($yacht->name); ?>
+</h5>
+
+<p><strong>Location:</strong> <?php echo esc_html($yacht->home_port); ?></p>
+
+<p>
+<?php echo esc_html($yacht->length_feet); ?>FT
+<?php echo esc_html($yacht->type); ?> -
+<?php echo esc_html($yacht->pax); ?> Guests
+</p>
+
+<p>Pricing From: €<?php echo esc_html($yacht->low_price); ?>/Week</p>
+
+<a href="/single-yacht-page/?usr_id_in=<?php echo esc_attr($yacht->yacht_code); ?>" class="btn btn-primary">
+VIEW YACHT
+</a>
+
+</div>
+</div>
+</div>
--- yacht-archive/yacht_archive.js
+++ yacht-archive/yacht_archive.js
@@ -1,15 +1,24 @@
 jQuery(document).ready(function($){
 
-$('#yacht_sort').change(function(){
+$('.yacht_filter').change(function(){
 
-var sort = $(this).val();
+var sort = $('#yacht_sort').val();
+var guest = $('#guest_filter').val();
+var location = $('input[name="yacht_location"]:checked').val();
+console.log('location:', location);
+
+
+
 
 $.ajax({
 url: ajax_object.ajax_url,
 type: "POST",
 data:{
 action:"yacht_sorting",
-sort:sort
+sort:sort,
+guests: guest,
+location: location
+
 },
 success:function(response){
 
@@ -22,3 +31,5 @@ $('#yacht_results').html(response);
 });
 
 });
+
+
